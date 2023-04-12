4500b2de776760997d5a2a3e7cc02deb6f8d2403
Nathan akpe <nathanakpe3@gmail.com>
2023-04-12 23:38:23 +0100

    (Week_2)

---
+++ Week_2/Practice_5.py
@@ -0,0 +1,18 @@
+# Returns false a game_1 not equal to game_2
+game_1 = 2
+game_2 = 4
+print(bool(game_1 == game_2))
+# Or
+print(game_1 == game_2)
+
+#Returns false as val is None
+val == None
+print(bool(val))
+
+# Returns false as num is an empty sequence
+num = ()
+print(bool(num))
+
+# Returns true as age is boolean
+age = True
+Print(bool(age))
