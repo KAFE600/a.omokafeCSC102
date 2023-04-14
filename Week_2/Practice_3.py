9ddd41d155f931b2a56bb3d0edc87ef5695f47df
KAFE600 <116589565+KAFE600@users.noreply.github.com>
2023-04-14 11:36:18 +0100

    Delete Week_2 directory

--- Week_2/Practice_3.py
+++
@@ -1,9 +0,0 @@
-list = ['Anaconda', '786[]
-shortlist = [321, 'Python']
-
-print(list) '' Prints complete list ''
-print(list[0])   #print first elements of the list
-print(list[1:3])     #print elements staring from 2nd till 3rd
-print(list[2:])   ''Prints elements starting from 3rd element''
-print(shortlist * 2) #print list two times
-print(list + shortlist)  #print concatenated lists
