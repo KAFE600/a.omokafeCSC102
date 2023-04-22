10d0ada2d6a920dce406f1274a6a630851b96ab2
Nathan akpe <nathanakpe3@gmail.com>
2023-04-22 23:50:45 +0100

    (Week_3)

---
+++ Week_3/Exercise_2.py
@@ -0,0 +1,9 @@
+#Program to check if a number is positive , Nwgative or 0
+
+num = float(input("Enter a number: "))
+if num > 0:
+    print("positive number")
+elif num == 0:
+print("Zero")
+else:
+print("negative number")
---
+++ Week_3/Project_1.py
@@ -0,0 +1,30 @@
+#THE  10 GIRLS
+Names1 = ['Samantha', 'Jada', 'Jane', 'Clarie', 'Elizabeth', 'Mary', 'Susan', 'Waje', 'Taibat', 'Lilian']
+Ages1 = [17,16,17,18,16,18,17,20,19,17]
+Heights1 = [5.5,6.0,5.4,5.9,5.6,5.5,6.1,6.0,5.7,5.5]
+Scores1 = [80,85,70,60,76,66,87,95,50,49]
+
+#THE  10 BOYS
+Names2 = ['Charles', 'Jude', 'James','Kelvin', 'Biodun', 'Wale', 'Kunle', 'Matthew', 'Tom', 'Kayode']
+Ages2 = [19,16,18,17,20,19,16,18,17,19]
+Heights2 = [5.7,5.9,5.8,6.1,5.9,5.5,6.1,5.4,5.8,5.7,]
+Scores2 = [74,87,75,68,66,78,87,98,54,60]
+
+for a in range(1):
+    print("Name", "Age", "Height", "Score")
+
+for a in range(9):
+    print(Names1[a], Ages1[a], Heights1[a], Scores1[a])
+
+for d in range(9):
+    print(Names2[d], Ages2[d], Heights2[d], Scores2[d])
+  
+
+
+
+
+  
+
+
+
+  
